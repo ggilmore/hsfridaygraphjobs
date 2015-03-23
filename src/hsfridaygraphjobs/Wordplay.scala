a9072a2b9ed779cc460468c31f88cbf2af01dce5
Geoffrey Gilmore <ggilmore@mit.edu>
2015-03-23 13:26:43 -0400

    implemented wordHash

--- src/hsfridaygraphjobs/Wordplay.scala
+++ src/hsfridaygraphjobs/Wordplay.scala
@@ -11,12 +11,12 @@ object Wordplay extends App{
 
   val lines = Source.fromFile(new File("src/resources/sowpods.txt")).getLines
 
-//  val anagramsThing:Map[Map[Char, Int],Set[WordLetters]] = lines.foldLeft(Map[Map[Char, Int], Set[WordLetters]]())(foo)
-//
-//  println((anagramsThing.filter(_._2.size > 1) map {case (m, wl) => m.foldLeft(0)(_+_._2) -> wl}).toIndexedSeq.sortBy(-_._1).take(1))
-//
-  val letterDictionary:Map[Char, WordLetters] = lines.foldLeft(Map[Char, WordLetters]())(foo2)
-  println((letterDictionary.toVector.sortBy(-_._2.word.length).take(1)))
+  val anagramsThing:Map[Map[Char, Int],Set[WordLetters]] = lines.foldLeft(Map[Map[Char, Int], Set[WordLetters]]())(foo)
+
+  println((anagramsThing.filter(_._2.size > 1) map {case (m, wl) => m.foldLeft(0)(_+_._2) -> wl}).toIndexedSeq.sortBy(-_._1).take(1))
+
+//  val letterDictionary:Map[Char, WordLetters] = lines.foldLeft(Map[Char, WordLetters]())(foo2)
+//  println((letterDictionary.toVector.sortBy(-_._2.word.length).take(1)))
 
   def foo(m: Map[Map[Char, Int], Set[WordLetters]], w:String) =  {
     if (w.nonEmpty){
@@ -52,6 +52,10 @@ case class WordLetters(word:String){
     var letterDictionary:Map[Char, Int] = ("abcdefghijljklmnopqurstuvwxyz" map (l => (l,0))).toMap
     word.foreach(l => letterDictionary = letterDictionary + (l -> (letterDictionary(l) + 1)))
     letterDictionary
+  }
+
+  val wordHash = {
+    letterCount.toIndexedSeq.sortBy(_._1).foldLeft(""){case (s, (l,c)) => s + s"$l + ${if (c <10) s"0$c" else c}"}
   }
 }
 
